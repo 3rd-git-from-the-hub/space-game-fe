6e184a3242f41842a3406615c1953811efb2d319
Erik Ford <ekford18@gmail.com>
2020-05-22 11:06:08 -0700

    Updates

---
+++ precache-manifest.04aa31a16af40d61e46e6e6fd05bda02.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a40a7406afafbdb137227cb009cdfa6",
+    "url": "/space-game-fe/index.html"
+  },
+  {
+    "revision": "b17dfa13fa6e0f713150",
+    "url": "/space-game-fe/static/css/main.76f186a5.chunk.css"
+  },
+  {
+    "revision": "3c7f0fa65a61adde28eb",
+    "url": "/space-game-fe/static/js/2.2b6e9dc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space-game-fe/static/js/2.2b6e9dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17dfa13fa6e0f713150",
+    "url": "/space-game-fe/static/js/main.11b633b9.chunk.js"
+  },
+  {
+    "revision": "d7376eb127e9c1161fb8",
+    "url": "/space-game-fe/static/js/runtime-main.eea6c7c7.js"
+  },
+  {
+    "revision": "863c8017ba57610c6c06ee06276dc625",
+    "url": "/space-game-fe/static/media/About-Us.863c8017.png"
+  },
+  {
+    "revision": "9b55bac792dd5a5945dcc3e3e3fb1127",
+    "url": "/space-game-fe/static/media/biology_rocket.9b55bac7.png"
+  },
+  {
+    "revision": "3319efd39d5e1660c0c02ec12674e2d5",
+    "url": "/space-game-fe/static/media/blackhole.3319efd3.png"
+  },
+  {
+    "revision": "8ccde6f21474ec32c65736f1dfdf7cf1",
+    "url": "/space-game-fe/static/media/borderimagetopleft.8ccde6f2.jpg"
+  },
+  {
+    "revision": "f83992f569fced8ebb3e6c911387b226",
+    "url": "/space-game-fe/static/media/combat_rocket.f83992f5.png"
+  },
+  {
+    "revision": "1700ba7318207f96ed29fd7c9401e66f",
+    "url": "/space-game-fe/static/media/diplomacy_rocket.1700ba73.png"
+  },
+  {
+    "revision": "49c3b412cfa448ec819412fb3ca089d2",
+    "url": "/space-game-fe/static/media/earthmap1k.49c3b412.jpg"
+  },
+  {
+    "revision": "f283b18bf936047e920ddba90a5a2fc4",
+    "url": "/space-game-fe/static/media/galaxy.f283b18b.jpg"
+  },
+  {
+    "revision": "0e4a8c3ef3ea33489b2a71383c4d8697",
+    "url": "/space-game-fe/static/media/goal_planet.0e4a8c3e.png"
+  },
+  {
+    "revision": "377771898dcb6b4ac7dd216bdaee780d",
+    "url": "/space-game-fe/static/media/meteroids.37777189.png"
+  },
+  {
+    "revision": "8e0eb78b40930d82be9c54cbd17fec9f",
+    "url": "/space-game-fe/static/media/mystery_planet.8e0eb78b.png"
+  },
+  {
+    "revision": "f37a23c5eb78a71a7daef93b62aeb014",
+    "url": "/space-game-fe/static/media/planetbackground.f37a23c5.jpg"
+  },
+  {
+    "revision": "8a101112f0a9c9acc4183d179d5532a9",
+    "url": "/space-game-fe/static/media/space_station.8a101112.png"
+  },
+  {
+    "revision": "904afa07c469d76c6b8c84ac2ebb543a",
+    "url": "/space-game-fe/static/media/testimagefortopleft.904afa07.jpg"
+  },
+  {
+    "revision": "afd0635d21ff26e72337c3f8bdd68721",
+    "url": "/space-game-fe/static/media/whiteCircle.afd0635d.png"
+  }
+]);
